3a6e45c866cbbfc3323d9d4a01ae92fffacfaa59
Frederick Hong <haurunis@outlook.com>
2023-09-11 09:35:09 +0000

    first update 328

--- python/328_Odd_Even_Linked_List.py
+++ python/328_Odd_Even_Linked_List.py
@@ -1,10 +1,20 @@
+import unittest
+
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode(object):
+    """ 
+    dd
+    """
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
 
 class Solution(object):
+    """
+    dd
+    """
     def oddEvenList(self, head):
         """
         :type head: ListNode
@@ -35,3 +45,58 @@ class Solution(object):
     #         head = head.next
     #     x.append(head.val)
     #     return x[0::2] + x[1::2]
+
+def create_linked_list(arr):
+    dummy = ListNode(0)
+    ptr = dummy
+    for num in arr:
+        ptr.next = ListNode(num)
+        ptr = ptr.next
+    return dummy.next
+
+def linked_list_to_list(head):
+    arr = []
+    while head:
+        arr.append(head.val)
+        head = head.next
+    return arr
+
+def test_example(arr, func):
+    head = create_linked_list(arr)
+    result = linked_list_to_list(func(head=head))
+    return result
+
+class Test(unittest.TestCase):
+    """
+    """
+    test_cases = [
+        ([2,1,3,5,6,4,7], [2,3,6,7,1,5,4])
+    ]
+    test_listnode_cases = []
+    test_functions = [Solution().oddEvenList]
+
+    # def prepare_linked_list(self, items: list) -> ListNode | None:
+    #     if not items:
+    #         return None
+    #     root = None
+    #     last_node = None
+    #     for ind, val in enumerate(items):
+    #         if ind == 0:
+    #             root = ListNode(val)  # root node
+    #             last_node = root
+    #         else:
+    #             last_node.next = ListNode(val)
+    #             last_node = last_node.next
+    #     return root
+
+    # in unittest, all func starts with "test" will be run by unittest.main(). Thus carefully choosing the name
+    def test_oddeven_link(self):
+        for (test_in, test_out) in self.test_cases:
+            for func in self.test_functions:
+                out = test_example(arr=test_in, func=func)
+                print(f'out is {out}')
+                assert (out == test_out), f"{func.__name__} fialed for val: {test_in}, as {out}"
+
+
+if __name__ == "__main__":
+    unittest.main()
