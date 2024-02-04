abeb92824fdb3a0acf0dc2d789ef03f9680faae3
HaFred <zhongad@connect.ust.hk>
2024-02-04 20:45:52 +0800

    * binary tree pre/in/post orders * substring detection using dp

--- python/032_Longest_Valid_Parentheses.py
+++ python/032_Longest_Valid_Parentheses.py
@@ -1,4 +1,6 @@
 import pdb
+
+
 class Solution(object):
     # def longestValidParentheses(self, s):
     #     """
@@ -41,7 +43,29 @@ class Solution(object):
                 tep = 0
         return max(tep, res)
 
+    def fred_brute_force(self, s):
+        def is_valid(x):
+            """A sub func to determine the current substr is valid for parenthesis construction or not, in outer descending iterations of substrings"""
+            stack = []
+            for i in range(len(x)):
+                if x[i] == '(':
+                    stack.append('(')
+                elif stack != [] and stack[-1] == '(':  # when x[i] == ')' and last ele is '(', it matches parenthesis
+                    stack.pop()
+                else:  # if stack[-1] == ')'
+                    return False  # coz it's gonna be a parenthesis with a lonely ')' here
+            return stack == []  # if there is remaining symbol in stacks meaning that current string contains invalid ( or ), thus False. Only empty list true
+
+        if len(s) < 2: return 0
+        n = len(s)
+        for i in range(n if n%2==0 else n-1, 0, -2):  # start, stop, step
+            for j in range(n-i+1): # on the left hand side idx
+                if is_valid(s[j:j+i]):
+                    return i  # found the max valid substring
+        return 0 # if fail to find valid, return 0
+
 if __name__ == '__main__':
     s = Solution()
     # print s.longestValidParentheses(")(((((()())()()))()(()))(")
-    print(s.longestValidParentheses(')()())'))
+    # print(s.longestValidParentheses(')()())'))
+    print(s.fred_brute_force(')()())'))
--- python/144_Binary_Tree_Preorder_Traversal.py
+++ python/144_Binary_Tree_Preorder_Traversal.py
@@ -1,9 +1,10 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
 
 class Solution(object):
     # def __init__(self):
@@ -26,7 +27,7 @@ class Solution(object):
     #     self.preorderTraversalHelper(node.left)
     #     self.preorderTraversalHelper(node.right)
 
-    def preorderTraversal(self, root):
+    def preorderTraversal_prev(self, root: [TreeNode]):
         # stack
         if root is None:
             return []
@@ -41,3 +42,31 @@ class Solution(object):
                 stack.append(curr.left)
         return res
 
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        stack = []
+        self.fred_preorder_traversal_func(root)
+
+    def fred_preorder_traversal_func(self, node, stack):
+        """
+        binary tree (bt) traversal steps:
+        bt1: determine the traverse param: which is the current handling node and the global preorder node list
+        bt2: determine end condition: when it reaches null ptr, ends for this traverse iteration (here formally speaking is using recursion)
+        bt3: since it is preorder, takes middle-left-right
+        """
+        if node == null: return
+        stack.append(node.val)  # middle
+        self.fred_preorder_traversal_func(node.left, stack)  # left
+        self.fred_preorder_traversal_func(node.right.stack)
+
+
+input = [1, null, 2, 3]
+
+
+# for linked list or binary tree questions, if input is a list, then it needs to be adapted first before passed into the solution function
+def create_linked_list(arr):
+    dummy = TreeNode(0)
+    ptr = dummy
+    for num in arr:
+        ptr.next = TreeNode(num)  # create linked node for each coming .next
+        ptr = ptr.next
+    return dummy.next
