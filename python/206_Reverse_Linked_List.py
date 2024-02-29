b4a6e8806fe04e13bca6715f2f040f8d99973669
HaFred <zhongad@connect.ust.hk>
2024-02-29 11:52:23 +0800

    linked list init and reverse it

--- python/206_Reverse_Linked_List.py
+++ python/206_Reverse_Linked_List.py
@@ -61,11 +61,48 @@ class Solution(object):
     #     return prev
 
     def reverseList(self, head):
+        """ steps of doing recursion
+        1. define end state and return
+        2. determine where to recurse it in the function
+        3. determine what is the return value for the recursion line
+        4. determine the real func here that helps the recursion works
+        """
         # recursion
         # simple recursively without extra space
         if head is None or head.next is None:
             return head
-        p = self.reverseList(head.next)
-        head.next.next = head
-        head.next = None
-        return p
+        new_head = self.reverseList(head.next)
+        head.next.next = head  # the original next node (head.next) now has a new next, which is the prev one, thus reverse the linked list
+        head.next = None  # delete the original direction of link
+        return new_head  # return the new head of the Linked List
+
+    def reverse_list_with_double_ptr(self, head):
+        # iterative double ptr
+        left_ptr, right_ptr = None, head
+        while right_ptr:
+            temp = right_ptr.next
+            right_ptr.next = left_ptr  # change the link direction, from left to right to right to left
+            # update the ptr position
+            left_ptr = right_ptr
+            right_ptr = temp
+
+        return left_ptr  # right ptr already None when reaches here, left ptr is the new head
+
+
+
+if __name__ == '__main__':
+    from python.design_linked_list_707 import MyLinkedList
+    input_head = [1, 2, 3, 4, 5]
+    output_head = [5, 4, 3, 2, 1]
+    test_node = ListNode(input_head[0])
+    test_node.next = ListNode(input_head[1])
+    input_linked_list = MyLinkedList()
+    for i in range(len(input_head)):
+        input_linked_list.addAtIndex(i, input_head[i])
+    solution = Solution()
+    # out = solution.reverseList(input_linked_list.dummy_head)
+    out = solution.reverse_list_with_double_ptr(input_linked_list.dummy_head)
+    for i in range(len(input_head)):
+        val = out.val
+        out = out.next
+        print(val)
